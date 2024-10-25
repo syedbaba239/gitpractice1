63fc015c90a2768d2a74fb4da6e0930eb9a18b73
Geetha <danda.saigeetha@wipro.com>
2024-10-25 17:19:43 +0000

    Added Helloworld.java, Hellojenkins and Hellowipro to repo

---
+++ Hellowipro.java
@@ -0,0 +1,6 @@
+class Hellowipro{
+public static void main(String args[])
+{
+ System.out.println("Hello Wipro");
+}
+}
--- Mypower.java
+++
@@ -1,6 +0,0 @@
-public class Mypower{
-public static void main(String args[])
-{
-System.out.println("My power is being Batman");
-}
-}
